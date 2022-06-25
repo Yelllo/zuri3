afdf3b8b91552e563829cce843ba3a3e48fecc58
tobenna <tobytkid@gmail.com>
2022-06-25 06:58:41 +0100

    first commit

---
+++ tasker.js
@@ -0,0 +1,27 @@
+//Declare the variables
+const operatorVar = prompt("Entr the operator that you want to use(*, -, +, /)");
+const number1 = prompt("Enter the first number");
+const number2 = prompt("Enter the second number");
+//Check for invalid inputs
+
+//Executing if and else statement
+if(operatorVar == "*"){
+    result = number1 * number2;
+    alert("The result is : " + result);
+}
+
+else if(operatorVar == "/"){
+    result = number1 / number2; 
+    alert("The result is : " + result);
+}
+else if(operatorVar == "-"){
+    result = number1 - number2; 
+    alert("The result is : " + result);
+}  
+else if(operatorVar == "+"){
+    result = parseInt(number1) + parseInt(number2);
+    alert("The result is : " + result);
+}
+else{
+    alert("Invalid operator");
+}
